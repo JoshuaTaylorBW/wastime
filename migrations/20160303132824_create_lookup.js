a612b0a625f6eb3ff2d01961992abd5243e61375
Joshua Sparks <JoshuaTaylorBW@gmail.com>
2016-03-03 15:48:15 -0700

    added shit

--- migrations/20160303132824_create_lookup.js
+++ migrations/20160303132824_create_lookup.js
@@ -1,5 +1,5 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('time_waster', function(table) {
+  return knex.schema.createTable('posted_by', function(table) {
     table.increments('id');
     table.text('submitted_by');
     table.text('id_of_submitted');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router()
+var knex = require('../db/knex');
+function TimeWasters() {
+  return knex('time_waster');
+};
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  TimeWasters().then(function (results) {
+    res.json(results);
+  })
+});
+
+router.get('/s/:searchTerm', function(req, res, next) {
+  console.log(req.params.searchTerm + "&&&&&&&&&&&&");
+  TimeWasters().where('name', '~*', '.*'+req.params.searchTerm+'%*.').then(function (results) {
+    console.log(results);
+    res.json(results);
+  })
 });
 
 module.exports = router;
